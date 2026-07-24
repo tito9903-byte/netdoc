a7f427eceb309570a13d4b5227faca3f8c4c7732
tito9903-byte <tito9903@gmail.com>
2026-07-24 15:46:08 -0400

    test: cover calculated pool availability

--- tests/test_documentation_workflows.py
+++ tests/test_documentation_workflows.py
@@ -9,6 +9,8 @@ from app.services.device_type_service import (
     slugify,
 )
 from app.services.ipam_service import (
+    build_occupancy_indexes,
+    calculate_pool_availability,
     prefix_capacity,
     scope_label,
     utilization_percentage,
@@ -78,6 +80,104 @@ class DocumentationWorkflowTests(unittest.TestCase):
         self.assertEqual(utilization_percentage(140), 100.0)
         self.assertEqual(utilization_percentage(-10), 0.0)
         self.assertIsNone(utilization_percentage("sin dato"))
+
+    def test_pool_availability_combines_addresses_ranges_and_child_prefixes(self):
+        pool = {
+            "id": 1,
+            "prefix": "192.0.2.0/29",
+            "is_pool": True,
+            "vrf": {"id": 3},
+        }
+        prefixes = [
+            pool,
+            {
+                "id": 2,
+                "prefix": "192.0.2.6/31",
+                "vrf": {"id": 3},
+            },
+        ]
+        addresses = [
+            {"address": "192.0.2.1/29", "vrf": {"id": 3}},
+            {"address": "192.0.2.2/29", "vrf": {"id": 3}},
+            {"address": "192.0.2.3/29", "vrf": {"id": 99}},
+        ]
+        ranges = [
+            {
+                "start_address": "192.0.2.4/29",
+                "end_address": "192.0.2.5/29",
+                "vrf": {"id": 3},
+                "mark_populated": True,
+            }
+        ]
+        indexes = build_occupancy_indexes(
+            ip_addresses=addresses,
+            ip_ranges=ranges,
+            prefixes=prefixes,
+        )
+
+        used, available, utilization = calculate_pool_availability(
+            pool,
+            address_intervals=indexes[0],
+            reserved_ranges=indexes[1],
+            prefix_intervals=indexes[2],
+        )
+
+        self.assertEqual(used, 6)
+        self.assertEqual(available, 2)
+        self.assertEqual(utilization, 75.0)
+
+    def test_unmarked_range_does_not_reserve_all_addresses(self):
+        pool = {
+            "id": 1,
+            "prefix": "198.51.100.0/30",
+            "is_pool": True,
+        }
+        indexes = build_occupancy_indexes(
+            ip_addresses=[{"address": "198.51.100.1/30", "vrf": None}],
+            ip_ranges=[{
+                "start_address": "198.51.100.0/30",
+                "end_address": "198.51.100.3/30",
+                "vrf": None,
+                "mark_populated": False,
+                "mark_utilized": False,
+            }],
+            prefixes=[pool],
+        )
+
+        used, available, utilization = calculate_pool_availability(
+            pool,
+            address_intervals=indexes[0],
+            reserved_ranges=indexes[1],
+            prefix_intervals=indexes[2],
+        )
+
+        self.assertEqual(used, 1)
+        self.assertEqual(available, 3)
+        self.assertEqual(utilization, 25.0)
+
+    def test_mark_utilized_pool_is_full(self):
+        pool = {
+            "id": 1,
+            "prefix": "203.0.113.0/30",
+            "is_pool": True,
+            "mark_utilized": True,
+        }
+        indexes = build_occupancy_indexes(
+            ip_addresses=[],
+            ip_ranges=[],
+            prefixes=[pool],
+        )
+
+        used, available, utilization = calculate_pool_availability(
+            pool,
+            address_intervals=indexes[0],
+            reserved_ranges=indexes[1],
+            prefix_intervals=indexes[2],
+        )
+
+        self.assertEqual(used, 4)
+        self.assertEqual(available, 0)
+        self.assertEqual(utilization, 100.0)
 
     def test_cable_endpoint_label_combines_device_and_interface(self):
         label = ConnectionService._object_label({
